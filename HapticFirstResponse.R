7d5840c0d89c586b99d05b5511f801c8fc726e8f
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-06-20 17:06:23 -0500

    add findAlertTime

--- HapticFirstResponse.R
+++ HapticFirstResponse.R
@@ -9,10 +9,21 @@ disp <- readxl::read_excel("H:\\disposition.xls")
 rand <- readxl::read_excel("H:\\MainTestMatrix.xlsx")
 
 
-## Filter 40Hz Intetensity Level Participants
-# Get participant number of 40Hz
-group40 <- rand[which(rand$`AlertLevel` == 2),]$'Participant#' 
+## For each participant, we will get the participant first response
+## find haptic alert start time
+findAlert <- function (data) {
+  haptic <- which(rownames(data$elemDataI) == 'SCC.Haptic.Alert')
+  h <- data$elemDataI[[haptic]][,1]
+  for (alert in 1: 13349){
+    if (h[alert] != 0){
+      alert <- numeric(alert)
+      break
+    }
+  }
+  return (alert)
+}
 
-## Get file names corresponding to participant number
-file40 <- filter(disp, ((substr(disp$'DaqPath', 2, 5) %in% group40) && 
-                          (substr(disp$'DaqPath', -2, -1) == "EV")))
+for (i in (1:30)){
+  test <- readMat(paste0("/Users/seoyeon/Documents/2019MAP/MatFiles/",file40[i]))
+  start <- findAlert (test)
+}
