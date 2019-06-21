d15b3ba48454cc87e425ba9de3fe6f96247de483
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-06-20 19:14:07 -0500

    filtering updated

--- HapticFirstResponse.R
+++ HapticFirstResponse.R
@@ -1,13 +1,10 @@
-#install.packages("R.matlab")
-library(R.matlab) # Reads matlab files
-library(stringr) # String manipulation
-library(tidyverse)
+## Filter 40Hz Intetensity Level Participants
+# Get participant number of 40Hz
+group40 <- rand[which(rand$`AlertLevel` == 2),]$'Participant#' 
 
-## Grab all of the .mat Files from directory H:\V2VMAT\MatFiles
-files <- list.files("H:\\V2VMAT\\MatFiles")
-disp <- readxl::read_excel("H:\\disposition.xls")
-rand <- readxl::read_excel("H:\\MainTestMatrix.xlsx")
-
+# Get file names corresponding to participant number
+file40 <- filter(disp, substr(disp$'DaqPath', 2, 5) %in% group40) 
+file40 <- filter(file40, substr(file40$'DaqPath', nchar(file40$'DaqPath') -1, nchar(file40$'DaqPath')) == "MN")$'DaqName'
 
 ## For each participant, we will get the participant first response
 ## find haptic alert start time
