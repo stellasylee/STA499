504adafcd205aed8c0b5965e2b06fc328aaecdaa
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-06-20 19:15:43 -0500

    Update HapticFirstResponse.R

--- HapticFirstResponse.R
+++ HapticFirstResponse.R
@@ -1,3 +1,13 @@
+#install.packages("R.matlab")
+library(R.matlab) # Reads matlab files
+library(stringr) # String manipulation
+library(tidyverse)
+
+## Grab all of the .mat Files from directory H:\V2VMAT\MatFiles
+files <- list.files("H:\\V2VMAT\\MatFiles")
+disp <- readxl::read_excel("H:\\disposition.xls")
+rand <- readxl::read_excel("H:\\MainTestMatrix.xlsx")
+
 ## Filter 40Hz Intetensity Level Participants
 # Get participant number of 40Hz
 group40 <- rand[which(rand$`AlertLevel` == 2),]$'Participant#' 
