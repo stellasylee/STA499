c09142879bc883f66dae638c437910b43ded8059
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-06-24 13:51:52 -0500

    Create haptic.R

---
+++ haptic.R
@@ -0,0 +1,10 @@
+library ("readxl")
+
+# Upload Data ----
+haptic <- read_excel("H:\\haptic\\output_v27.xls")
+## V2V Safety Systems Forward Collision Warning event
+FCW <- read_excel("H:\\haptic\\FCWCompletes_AllSites_EditThis.xlsx")
+## V2V Safety Systems Left Turn and Intersection Movement Assist events
+IMLT <- read_excel("H:\\haptic\\IMLTCombinedData.xlsx")
+
+
