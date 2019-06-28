011cf0ae880932049e1afd3be1dae8356b99ae89
IshaanTibrewal <51420378+IshaanTibrewal@users.noreply.github.com>
2019-06-28 15:24:51 -0500

    Update standarddev.R

--- NIDA study/standarddev.R
+++ NIDA study/standarddev.R
@@ -5,13 +5,50 @@ install.packages("R.matlab")
 install.packages("doParallel")
 install.packages("foreach")
 install.packages("rio")
+install.packages("changepoint")
 
 library(rio)
 library(R.matlab) # Reads matlab files
 library(stringr) # String manipulation
 library(readxl) 
 library(dplyr)
+library(stringr) # String manipulation
+library(tidyverse)
+library (changepoint)
 
+
+
+
+#trying to figure out how you find which rows would comprise the control group if we have the experimental group
+#start <- (start of experimental group -(length(end experimental - start experimental)))
+#end <- (start of the experimental group)
+
+detectTime <- function (file){
+  valid <- FALSE # Some simulation had non-zero value at the start
+  # start = when task was available
+  for (start in 1: nrow(file)){
+    if ((valid == TRUE) && (file$SCC.MenuSearch[start] != 0)){
+      break
+    }else if (file$SCC.MenuSearch[start] == 0){
+      valid <- TRUE
+    }
+  }
+  
+  valid <- FALSE
+  # end = when task was completed
+  for (end in (start+1): nrow(file)){
+    if ((valid == TRUE) && (file$AUX1.MenuScore[end] == 1)){
+      break
+    }else if (file$AUX1.MenuScore[end] == 0){
+      valid <- TRUE
+    }
+  }
+  return (c(start, end))
+}
+
+detectEngagement <- function (start, end){
+  
+}
 
 #testfile for checking how to find standard devation 
 tvar <- c("ScenarioOrder", "Subject")
@@ -20,6 +57,10 @@ sss <- 5
 eee <- 10
 rowstouse <- sss:eee
 sd(sdtest$ScenarioOrder[rowstouse])
+
+sdonefile <- sd(m1$SCC.Lane.Deviation.2[detectTime(m1)])
+
+sdonefile
 
 #code to calculate sd would be
 #sd(m1$SCC.Lane.Deviation.2[start:end])
@@ -32,6 +73,28 @@ sd(sdtest$ScenarioOrder[rowstouse])
 #}
 #}
 
-#trying to figure out how you find which rows would comprise the control group if we have the experimental group
-#start <- (start of experimental group -(length(end experimental - start experimental)))
-#end <- (start of the experimental group)
+sdonefile <- sd(m1$SCC.Lane.Deviation.2[detectTime(m1)])
+
+#function for standard deviation
+sdlanedev <- function (file){
+  return (sd(file$SCC.Lane.Deviation.2[detectTime(file)]))
+}
+
+
+for (i in 1:length(files)){
+  testfile <- read.csv(paste0("H:\\CannabisStudy\\CSV\\",files[i]))
+  output <- sdlanedev(testfile)
+  print(output)
+}
+
+#function for mean speed
+meanspeed <- function(file){
+  return(mean(file$VDS.Veh.Speed[detectTime(file)]))
+}
+
+for (i in 1:length(files)){
+  testfile <- read.csv(paste0("H:\\CannabisStudy\\CSV\\",files[i]))
+  output <- meanspeed(testfile)
+  print(output)
+}
+
