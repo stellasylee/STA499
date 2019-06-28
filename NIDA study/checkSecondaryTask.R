684608c2273ffed6fe17d723b6bf6f542eecba41
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-06-28 11:56:36 -0500

    Update checkSecondaryTask.R

--- NIDA study/checkSecondaryTask.R
+++ NIDA study/checkSecondaryTask.R
@@ -8,18 +8,16 @@ disp <- readxl::read_excel("H:\\NIDA\\disposition.xls")
 # filter ignore/discard data
 disp <- filter (disp, is.na(Ignore)) %>% filter(., is.na(Discard))
 
-dispRNA <- filter(disp, Restart =='RNA')
-fileNames <- str_replace(dispRNA$DaqName, ".daq", ".csv")
+
+fileNames <- str_replace(disp$DaqName, ".daq", ".csv")
+noevents <- c()
 for (i in 1:length(fileNames)){
   tempFile <- read.csv(paste0("H:\\NIDA\\ReducedCSV\\", fileNames[i]))
-  if (length(unique(tempFile$SCC.MenuSearch)) != 4){
-    print(fileNames[i])
+  if ((length(unique(tempFile$SCC.MenuSearch)) != 4)&&(length(unique(tempFile$SCC.MenuSearch)) != 5)){
+    noevents <- append (noevents, fileNames[i])
   }
 }
 
-
-tempFile <- read.csv(paste0("H:\\NIDA\\ReducedCSV\\", "20130206102259.csv"))
-if (length(unique(tempFile$SCC.MenuSearch)) != 4)
-  print(files[i])
-length(unique(tempFile$SCC.MenuSearch))
-unique(tempFile$SCC.MenuSearch)
+disp$DaqName <- str_replace(disp$DaqName, ".daq", ".csv")
+filteredDisp <- filter (disp, !(DaqName %in% noevents))
+# drop red case
