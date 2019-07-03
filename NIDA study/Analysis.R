f053530850be88e44b03279e552589e4f9559f01
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-07-03 13:44:46 -0500

    Update Analysis.R

--- NIDA study/Analysis.R
+++ NIDA study/Analysis.R
@@ -4,9 +4,16 @@ library ("dplyr")
 library(lme4)
 
 analysisfile <- read.csv("H:\\NIDA\\analysisMatrix.csv")
+analysisfile <- read.csv("H:\\NIDA\\analysisWithoutEngagementPoint.csv")
 analysisfile <- analysisfile[order(analysisfile$ID, analysisfile$Experiment),]
 
 fit1 <- lmer(SD.Lane.Deviation ~ Experiment + factor(DosingLevel) + (1 | ID), data = analysisfile)
 summary(fit1)
 
-fit3 <- lmer(SD.Lane.Deviation ~ Experiment + factor(DosingLevel) + Experiment:factor(DosingLevel) + (1 | ID), data = analysisfile)
+analysisfile$Experiment <- as.factor(analysisfile$Experiment)
+analysisfile$DosingLevel <- as.factor(analysisfile$DosingLevel)
+fit3 <- lmer(SD.Lane.Deviation ~ Experiment + DosingLevel + Experiment:DosingLevel + (1 | ID), data = analysisfile)
+summary(fit3)
+
+coef(fit3)
+plot(analysisfile$SD.Lane.Deviation)
