c1167b6389c254c31ee4e24b5ce61cc41e09e33f
IshaanTibrewal <51420378+IshaanTibrewal@users.noreply.github.com>
2019-07-15 15:00:31 -0500

    Update artistTask_initialAnalysis.R
    models created for artist task

--- NIDA study/Analysis/artistTask_initialAnalysis.R
+++ NIDA study/Analysis/artistTask_initialAnalysis.R
@@ -1,7 +1,6 @@
 install.packages("readxl")
 install.packages("tidyverse")
 install.packages("dplyr")
-install.packages("foreach")
 install.packages("lme4")
 install.packages("lmerTest")
 
@@ -10,25 +9,66 @@ library(readxl)
 library(dplyr)
 library(lme4)
 library(lmerTest)
+library(plotly)
 #reading in files
 
-AnalysisArtist <- read.csv("H:\\CannabisStudy\\validAnalysisArtist.csv")
-ArtistTimes <- read.csv("H:\\CannabisStudy\\validArtistTimes.csv")
+AnalysisArtist <- read.csv("H:\\CannabisStudy\\artist\\analysisArtist.csv")
+ArtistTimes <- read.csv("H:\\CannabisStudy\\artist\\artistTimes.csv")
 
 View(AnalysisArtist)
 
-#filtering to keep only valid artist
 
 
+#checking if THC + BAC groups got more incorrect 
+#### fit <- glmer((valid == 0) ~ BAC + THC + factor(pageNum)  + (1 | ID), data = data2, family = "binomial")
+
+fit1 <- glm(data = AnalysisArtist, incorrect ~ THC , family = "binomial")
+summary(fit1)
+
+fit2 <- glm(data = AnalysisArtist, valid ~ THC, family = "binomial")
+summary(fit2)
+
+#filtering to keep only valid artist ###since they might not be engaged
 validArtist <- filter(AnalysisArtist, AnalysisArtist$valid > 0)
 
 View(validArtist)
+#filtering Analysis Artist to not include instances when they were not engaged i.e valid = and incorrect = 0
+
+keepingengagement <- filter (AnalysisArtist, !((valid == 0) & (incorrect == 0)))
+
+#modelling the data with keeping artist
+fit <- lmer(data = keepingengagement, log (SD.Lane.Deviation) ~ (1 | ID) + Experiment + DosingLevel + Avg.Speed + pageNum)
+summary(fit)
+#with this model Dosing level YM is a significant predictor
+
+#modelling keepingengagment data with avg speed as a predictor with Dosing Level
+fit <- lmer(data = keepingengagement, Avg.Speed ~ (1 | ID) + Experiment + DosingLevel + pageNum)
+summary(fit)
+
+#modelling keepingengagment data with avg speed as a predictor with THC and BAC
+fit <- lmer(data = keepingengagement, Avg.Speed ~ (1 | ID) + Experiment + THC + BAC + pageNum)
+summary(fit)
+#THC and BAC are not good predictors here
+
+#modelling keepingengagment data with SD speed as a predictor with Dosing Level
+fit <- lmer(data = keepingengagement, SD.Speed ~ (1 | ID) + Experiment + DosingLevel + pageNum)
+summary(fit)
+#does not really predict anything
+
+#modelling keepingengagment data with Sd Speedas a predictor with THC and BAC
+fit <- lmer(data = keepingengagement, SD.Speed ~ (1 | ID) + Experiment + THC + BAC + pageNum)
+summary(fit)
+#this model does not predict much either
 
 
-#modelling the data 
+#modelling the data with filtered data to keep only valid artist
+fit <- lmer(data = validArtist, log(SD.Lane.Deviation) ~ (1 | ID) + Experiment + THC + BAC + Avg.Speed + pageNum)
+summary(fit)
 
+
+#initial model built with previous data 
 fit <- lmer(data = validArtist, log (SD.Lane.Deviation) ~ (1 | ID) + Experiment + THC + BAC + Avg.Speed + factor (pageNum), control = lmerControl(optCtrl = list(maxiter = 30000)) )
-
+summary(fit)
 
 #checking residual plots of this model
 
@@ -49,5 +89,4 @@ summary(fit)
 
 fit <- lmer(data = validArtist,  SD.Speed ~ (1 | ID) + Experiment + THC + BAC )
 summary(fit)
-
 
