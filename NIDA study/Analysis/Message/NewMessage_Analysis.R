a408937fc6b8b85ce0f0a8c40f3c79e9b8a3203b
IshaanTibrewal <51420378+IshaanTibrewal@users.noreply.github.com>
2019-07-19 14:54:43 -0500

    Update NewMessage_Analysis.R

--- NIDA study/Analysis/Message/NewMessage_Analysis.R
+++ NIDA study/Analysis/Message/NewMessage_Analysis.R
@@ -4,7 +4,10 @@ install.packages("dplyr")
 install.packages("foreach")
 install.packages("lme4")
 install.packages("lmerTest")
+install.packages("lqmm")
 
+library(MASS)
+library(lqmm)
 library(stringr) # String manipulation
 library(readxl) 
 library(dplyr)
@@ -13,7 +16,7 @@ library(lmerTest)
 #reading in files
 
 eventTimesMessage <- read.csv("H:\\CannabisStudy\\message\\eventTimesMessage.csv")
-analysisMes <- read.csv("H:\\CannabisStudy\\message\\analysisMes.csv")
+analysisMes <- read.csv("H:\\CannabisStudy\\message\\analysisMesBrake.csv")
 
 #filtering out and creating separate dataframes for experimental and control groups
 
@@ -28,11 +31,14 @@ MessageControl <- arrange(MessageControl, desc(DaqName, eventNum))
 
 MessageExperiment <- mutate(MessageExperiment, SD.Lane.Diff = MessageExperiment$SD.Lane.Deviation - MessageControl$SD.Lane.Deviation,
                            Avg.Speed.Diff = MessageExperiment$Avg.Speed - MessageControl$Avg.Speed, 
-                           SD.Speed.Diff = MessageExperiment$Sd.Speed - MessageControl$Sd.Speed)
+                           SD.Speed.Diff = MessageExperiment$Sd.Speed - MessageControl$Sd.Speed,
+                           Break.Diff = MessageExperiment$Max.Brake - MessageControl$Max.Brake)
+
 
 #Modelling the data with Message Experiment
 
 #lane deviation
+
 mesfit <- lmer(data = MessageExperiment, SD.Lane.Diff ~ (1 | ID) + THC + BAC + Avg.Speed + factor(LogStreams.5))
 summary(mesfit)
 
@@ -43,3 +49,28 @@ summary(mesfit)
 #standard deviation speed
 mesfit <- lmer(data = MessageExperiment, SD.Speed.Diff ~ (1 | ID) + THC + BAC + factor(LogStreams.5)) 
 summary(mesfit)
+AIC(mesfit)
+
+#running mixed effects quantile regression
+#lane deviation
+mesfit <- lqmm(SD.Lane.Diff ~ THC + BAC + Avg.Speed + factor(LogStreams.5), random = ~ 1, group = ID, data = MessageExperiment,
+               tau = 0.75)
+summary(mesfit)
+
+#Avg Speed
+mesfit <- lqmm(Avg.Speed.Diff ~ THC + BAC + Avg.Speed + factor(LogStreams.5), random = ~ 1, group = ID, data = MessageExperiment,
+               tau = 0.75)
+summary(mesfit)
+
+#SD speed
+mesfit <- lqmm(SD.Speed.Diff ~ THC + BAC + Avg.Speed + factor(LogStreams.5), random = ~ 1, group = ID, data = MessageExperiment,
+               tau = 0.75)
+summary(mesfit)
+
+
+#max brake force
+mesfit <- lqmm(Break.Diff ~ THC + BAC + Avg.Speed, random = ~ 1, group = ID, data = MessageExperiment,
+                      tau = 0.75)
+summary(mesfit)               
+  
+
