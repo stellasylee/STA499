df7e43eac5e6d4b14c3df0565c5ae9e28e1b4e3f
IshaanTibrewal <51420378+IshaanTibrewal@users.noreply.github.com>
2019-07-24 15:27:25 -0500

    Update NewArtist_Analysis.R

--- NIDA study/Analysis/Artist/NewArtist_Analysis.R
+++ NIDA study/Analysis/Artist/NewArtist_Analysis.R
@@ -70,6 +70,7 @@ summary(mesfit)
 
 validArtist <- dplyr::filter(ArtistExperiment, ArtistExperiment$valid > 0)
 
+
 #modelling with median as well
 #Lane deviation with median
 fit <- lqmm(SD.Lane.Diff ~ THC + BAC + Avg.Speed + factor(pageNum), random = ~1, group = ID, 
@@ -126,6 +127,7 @@ fit <- lmer(data = validArtist, Avg.Speed.Diff ~ (1 | ID) + log(THC + 1) + BAC +
 summary(fit)
 
 ##Log(THC + 1 with median)
+
 
 fit <- lqmm(Avg.Speed.Diff ~ log(THC + 1) + BAC + experimentallength + factor(pageNum), random = ~1, group = ID, 
             data = validArtist, tau = 0.5)
@@ -198,10 +200,9 @@ summary(fit)
 #average speed with median
 
 fit <- lqmm(Avg.Speed.Diff ~ THC + BAC + experimentallength + factor(pageNum), random = ~1, group = ID, 
-            data = validArtist, tau = 0.5)
+            data = validArtist, tau = 0.5, control = lqmmControl(LP_max_iter = 10000))
 summary(fit)
-####### THC significant with positive coefficient#############
-
+####### THC significant with positive coefficient############# 
 
 #two THC groups with median
 
@@ -256,3 +257,69 @@ summary(fit)
 fit <- lqmm(SD.Speed.Diff ~ log(THC + 1) + BAC + experimentallength + factor(pageNum), random = ~1, group = ID, 
             data = validArtist, tau = 0.5)
 summary(fit)
+
+#looking at the plots
+ggplot(data = validArtist, aes (x = THC, y = SD.Lane.Deviation, color = Avg.Speed)) + 
+  geom_point() + scale_color_continuous(low = "blue", high = "red") + facet_wrap(~ ID, scales = 'free') + geom_smooth()
+
+
+
+#people with higher levels of THC required longer to complete the artist task
+ggplot(data = validArtist, aes (x = THC, y = experimentallength)) + 
+  geom_point()  
+
+
+#Clustering
+
+THCrangeart <- NULL
+for (i in unique(validArtist$ID)){
+  temp <- filter(validArtist, ID == i)
+  THCrangeart <- rbind.data.frame(THCrangeart, c(i, max(temp$THC), min(temp$THC), (max(temp$THC) - min(temp$THC))))
+}
+colnames(THCrangeart) <- c("ID", "max", "min", "range")
+rownames(THCrangeart) <- THCrangeart$ID
+k2 <- kmeans(THCrangeart[, 4], centers = 2, nstart = 25)
+fviz_cluster(k2, data = THCrangeart)
+# Cluster 1: Participant 7, 18, 29, 34, 35, 123, 129
+k3 <- kmeans(THCrangeart[, 4], centers = 3, nstart = 25)
+fviz_cluster(k3, data = THCrangeart)
+# Cluster 1: Participant 7, 18
+# cluster 2: Participant 25, 26, 29, 32, 34, 35, 123, 129
+
+#Creating the clusters (NOTE: These clusters are created using valid artist)
+clus1art <- dplyr::filter(validArtist, ID == "7" | ID == "18" | 
+                         ID == "29" | ID == "34" | ID == "120" | ID == "123")
+
+clus2art <- dplyr::filter(validArtist, ID == "3" | ID == "10" | ID == "15" | ID == "17" |
+                         ID == "15" | ID == "21" | ID == "25" | ID == "26" | ID == "31" | ID == "32"
+                       | ID == "35" | ID == "104" | ID == "113" | ID == "129")
+
+#creating models with the clusters
+
+#Lane Deviation
+fit <- lmer(data = clus1art, SD.Lane.Diff ~ (1 | ID) + ns(THC,3) + BAC + ns(Avg.Speed,3) + factor(pageNum))
+summary(fit)
+anova(fit)
+AIC(fit)
+
+fit <- lmer(data = clus2art, SD.Lane.Diff ~ (1 | ID) + ns(THC,3) + BAC + ns(Avg.Speed,3) + factor(pageNum))
+summary(fit)
+anova(fit)
+
+#Avg. Speed
+fit <- lmer(data = clus1art, Avg.Speed.Diff ~ (1 | ID) + ns(THC,3) + BAC  + factor(pageNum))
+summary(fit)
+anova(fit)
+
+fit <- lmer(data = clus2art, Avg.Speed.Diff ~ (1 | ID) + ns(THC,3) + BAC  + factor(pageNum))
+summary(fit)
+anova(fit)
+
+#SD.Speed
+fit <- lmer(data = clus1art, SD.Speed.Diff ~ (1 | ID) + ns(THC,3) + BAC  + factor(pageNum))
+summary(fit)
+anova(fit)
+
+fit <- lmer(data = clus2art, SD.Speed.Diff ~ (1 | ID) + ns(THC,3) + BAC  + factor(pageNum))
+summary(fit)
+anova(fit)
