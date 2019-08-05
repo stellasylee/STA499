5d95463d417ce657c63d4e4df94e6cfbf70a0c00
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-08-05 14:38:30 -0500

    add artist task result

--- NIDA study/Paper/DosingLevelTable.R
+++ NIDA study/Paper/DosingLevelTable.R
@@ -91,3 +91,34 @@ summarize(mirrorRural,
           SDLD = mean(SD.Lane.Deviation, na.rm = TRUE), 
           Speed = mean(Avg.Speed, na.rm = TRUE),
           SDS = mean(SD.Speed, na.rm = TRUE))
+
+# Artist Task ====
+artist <- read.csv("H:\\NIDA\\finalArtist.csv")
+artistExp <- filter (artist, Experiment == 1)
+
+prop.table(table(artistExp$DosingLevel, artistExp$valid), 1)
+prop.table(table(artistExp$valid))
+
+prop.table(table(artistExp$DosingLevel, artistExp$incorrect), 1)
+prop.table(table(artistExp$incorrect))
+
+mean(artistExp$THC)
+mean(artistExp$BAC)
+artistByDosingLevel <- group_by(artistExp, DosingLevel)
+summarize(artistByDosingLevel, 
+          THC = mean(THC, na.rm = TRUE), 
+          BAC = mean(BAC, na.rm = TRUE))
+
+complete <- filter(artistExp, valid == 1)
+complete$time <- complete$total/60
+summarize(complete, 
+          time = mean(time, na.rm = TRUE),
+          SDLD = mean(SD.Lane.Deviation, na.rm = TRUE), 
+          Speed = mean(Avg.Speed, na.rm = TRUE),
+          SDS = mean(SD.Speed, na.rm = TRUE))
+artistByDosingLevel <- group_by(complete, DosingLevel)
+summarize(artistByDosingLevel, 
+          time = mean(time, na.rm = TRUE),
+          SDLD = mean(SD.Lane.Deviation, na.rm = TRUE), 
+          Speed = mean(Avg.Speed, na.rm = TRUE),
+          SDS = mean(SD.Speed, na.rm = TRUE))
