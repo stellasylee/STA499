def0db47efba2daf4c759f1b9abc427d83be391c
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-09-08 20:04:19 -0500

    Update LaneDeparture.R

--- NIDA study/Data processing/LaneDeparture.R
+++ NIDA study/Data processing/LaneDeparture.R
@@ -5,7 +5,9 @@ library(dplyr)
 # Artist Task ====
 artist <- read.csv("H:\\NIDA\\final\\finalArtist.csv")
 artistTimes <- read.csv ("H:\\NIDA\\final\\artistTimes.csv")
-artist[,"Lane.Departure"] <- rep(-1,nrow(artist))
+artist[,"Lane.Departure.Minor"] <- rep(-1,nrow(artist))
+artist[,"Lane.Departure.Major"] <- rep(-1,nrow(artist))
+artist[,"Lane.Departure.Severe"] <- rep(-1,nrow(artist))
 artist <- artist[order(artist$ID, artist$DosingLevel, artist$eventNum), ]
 artistTimes <- artistTimes[order(artistTimes$ID, artistTimes$DosingLevel, artistTimes$eventNum), ]
 fileNames <- artistTimes$DaqName
@@ -18,33 +20,54 @@ for (i in 1:length(fileNames)){
   start <- artistTimes$start[i]
   end <- artistTimes$end[i]
   duration <- (end - start)
-  count <- 0
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
+  
   for (j in start:end){
-    if (abs(file$SCC.Lane.Deviation.2[j]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  artist[index, "Lane.Departure"] <- as.numeric(count / duration * 100)
-  print(paste0(count, " ", (count / duration * 100)))
-  count <- 0
+  artist[index, "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  artist[index, "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  artist[index, "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
+  
   for (j in (start - duration):start){
-    if (abs(file$SCC.Lane.Deviation.2[i]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  artist[(index+1), "Lane.Departure"] <- as.numeric(count / duration * 100)
-  print((count / duration * 100))
+  
+  artist[(index+1), "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  artist[(index+1), "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  artist[(index+1), "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
   index <- index + 2
 }
 
-write.csv(artist, "H:\\NIDA\\final\\finalArtistLD.csv")
+write.csv(artist, "H:\\NIDA\\final\\finalFinalArtistLD.csv")
 
 # Message Reading ====
 message <- read.csv("H:\\NIDA\\final\\analysisMesBrake.csv")
 messagetimes <- read.csv("H:\\NIDA\\final\\eventTimesMessage.csv")
 message <- message[order(message$ID, message$DosingLevel, message$eventNum), ]
 messagetimes <- messagetimes[order(messagetimes$ID, messagetimes$DosingLevel, messagetimes$eventNum), ]
-message[,"Lane.Departure"] <- rep(-1,nrow(message))
+message[,"Lane.Departure.Minor"] <- rep(-1,nrow(message))
+message[,"Lane.Departure.Major"] <- rep(-1,nrow(message))
+message[,"Lane.Departure.Severe"] <- rep(-1,nrow(message))
 fileNames <- messagetimes$DaqName
 index <- 1
 for (i in 1:length(fileNames)){
@@ -55,22 +78,42 @@ for (i in 1:length(fileNames)){
   start <- messagetimes$start[i]
   end <- messagetimes$end[i]
   duration <- (end - start)
-  count <- 0
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
+  
   for (j in start:end){
-    if (abs(file$SCC.Lane.Deviation.2[j]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  message[index, "Lane.Departure"] <- as.numeric(count / duration * 100)
-  print(paste0(count, " ", (count / duration * 100)))
-  count <- 0
+  
+  message[index, "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  message[index, "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  message[index, "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
+  
   for (j in (start - duration):start){
-    if (abs(file$SCC.Lane.Deviation.2[i]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  message[(index+1), "Lane.Departure"] <- as.numeric(count / duration * 100)
-  print((count / duration * 100))
+  
+  message[(index+1), "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  message[(index+1), "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  message[(index+1), "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
   index <- index + 2
 }
 
@@ -79,7 +122,9 @@ write.csv(message, "H:\\NIDA\\final\\finalMessage.csv")
 # Side Mirror Reading ====
 mirror <- read.csv ("H:\\NIDA\\final\\analysisMirror.csv") # this already changed 299 frames to valid = 0 
 mirror <- dplyr::filter (mirror, total <= 300)
-mirror[,"Lane.Departure"] <- rep(-1,nrow(mirror))
+mirror[,"Lane.Departure.Minor"] <- rep(-1,nrow(mirror))
+mirror[,"Lane.Departure.Major"] <- rep(-1,nrow(mirror))
+mirror[,"Lane.Departure.Severe"] <- rep(-1,nrow(mirror))
 fileNames <- mirror$DaqName
 i <- 1
 while (i <= length(fileNames)){
@@ -91,30 +136,44 @@ while (i <= length(fileNames)){
   start <- mirror$start[i]
   end <- mirror$end[i]
   duration <- mirror$total[i]
-  count <- 0
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
   
   for (j in start:end){
-    if (abs(file$SCC.Lane.Deviation.2[j]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  mirror[i, "Lane.Departure"] <- as.numeric(count / duration * 100)
-  print(paste0("experiment done"))
-  if (count != 0){
-    print(paste0(count, " ", duration))
-  }
-  count <- 0
+  
+  mirror[i, "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  mirror[i, "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  mirror[i, "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
+  countMinor <- 0
+  countMajor <- 0
+  countSevere <- 0
+  
   for (j in (start - duration):start){
-    if (abs(file$SCC.Lane.Deviation.2[i]) > 6){
-      count <- count + 1
+    if (abs(file$SCC.Lane.Deviation.2[j]) >= 6){
+      countSevere <- countSevere + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 4.5){
+      countMajor <- countMajor + 1
+    }else if (abs(file$SCC.Lane.Deviation.2[j]) >= 3){
+      countMinor <- countMinor + 1
     }
   }
-  mirror[(i+1), "Lane.Departure"] <- as.numeric(count / duration * 100)
-  if (count != 0){
-    print(paste0(count, " ", duration))
-  }
+  
+  mirror[(i+1), "Lane.Departure.Minor"] <- as.numeric(countMinor / duration * 100)
+  mirror[(i+1), "Lane.Departure.Major"] <- as.numeric(countMajor / duration * 100)
+  mirror[(i+1), "Lane.Departure.Severe"] <- as.numeric(countSevere / duration * 100)
+  
   i <- i + 2
 }
 
-mirror$Lane.Departure[mirror$Lane.Departure > 100] <- 100
-write.csv(mirror, "H:\\NIDA\\final\\finalMirror.csv")
+mirror$Lane.Departure[mirror$Lane.Departure.Severe > 100] <- 100
+write.csv(mirror, "H:\\NIDA\\final\\finalFinalMirror.csv")
